2e346534192883597f5a2e12dc8e49ae85b5c156
mrbilby <baileymrb@gmail.com>
2023-03-26 13:51:26 +0100

    Initial Commit

---
+++ ExpenseTest/ExpenseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTestApp.swift
+//  ExpenseTest
+//
+//  Created by James Bailey on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
